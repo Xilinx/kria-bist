452f013f1b782e7f67491af37a09d7a4149cd420
Sagar Karmarkar <sagar.karmarkar@amd.com>
2023-02-22 10:11:58 -0800

    tests:gpio: Added GPIO Loopback Test
    Signed-off-by: Sagar Karmarkar <sagar.karmarkar@amd.com>

--- tests/gpio/gpio.py
+++ tests/gpio/gpio.py
@@ -1,18 +1,27 @@
-# Copyright (C) 2022 Advanced Micro Devices, Inc.
+# Copyright (C) 2022-2023 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: MIT
 
+# Import the 'modules' that are required for test cases execution
 import glob
+import itertools
 from periphery import GPIO
 
+
 def gpio_get_chip():
+    """
+    Find correct devpath for GPIO under test
+
+    Returns:
+            string: GPIO devpath
+    """
     chips = glob.glob('/dev/gpiochip*')
     for chip in chips:
         gpio = GPIO(chip, 0, "in")
         label = gpio.chip_label
         # Check for a label in format "<8-digit hex value>.gpio"
-        if(len(label)==13) and label.split('.')[1] == 'gpio':
+        if (len(label) == 13) and label.split('.')[1] == 'gpio':
             try:
-                int(label.split('.')[0],16)
+                int(label.split('.')[0], 16)
                 device_path = gpio.devpath
                 gpio.close()
                 return device_path
@@ -22,30 +31,115 @@ def gpio_get_chip():
             gpio.close()
     print('No gpiochip found with label in format "<8-digit hex value>.gpio"')
 
+
 def gpio_write(chip, offset, value):
     """
     Writes value (True or False) at given offset
+
+    Args:
+            chip: GPIO devpath
+            offset: Write offset, value written at this offset
+            value: Value to be written
     """
+    # Open legitimate GPIO out line, write the value, close GPIO line
     gpio_out = GPIO(chip, int(offset), "out")
-    gpio_out.write(value)
+    gpio_out.write(bool(value))
     gpio_out.close()
+
 
 def gpio_read(chip, offset):
     """
     Reads value at given offset
+
+    Args:
+            chip: GPIO devpath
+            offset: Read offset, value read from this offset
+
+    Returns:
+                int: Value read from given offset
     """
+    # Open legitimate GPIO in line, read the value, close GPIO line
     gpio_in = GPIO(chip, int(offset), "in")
-    read_val = gpio_in.read()
+    read_val = int(gpio_in.read())
     gpio_in.close()
     return read_val
 
-def run_gpio_loopback(width, offset):
+
+def gpio_write_range(chip, offset, width, values):
+    """
+    Args:
+            chip: GPIO devpath
+            offset: Write offset, value written at this offset
+            width: Number of bits to write
+            values: Tuple of int values
+    """
+    # Function call to write values to a GPIO line
+    for i in range(width):
+        gpio_write(chip, offset + i, values[i])
+
+
+def gpio_read_range(chip, offset, width):
+    """
+    Args:
+            chip: GPIO devpath
+            offset: Read offset,value read from this offset
+            width: Number of bits to read
+
+    Returns:
+                tuple: Tuple of int values
+    """
+    r_pattern = []
+
+    # Function call to read values from a GPIO line
+    for i in range(width):
+        r_pattern.append(gpio_read(chip, offset + i))
+    return tuple(r_pattern)
+
+
+def generate_patterns(width):
+    """
+    Generate bit patterns for all permutations of <width> number of bits
+
+    Args:
+            width: Number of bits
+
+    Returns:
+                list: List of tuples of type int with binary pattern
+    """
+    patterns = list(itertools.product([0, 1], repeat=int(width)))
+    return patterns
+
+
+def run_gpio_loopback(width, offset, logger):
     """
     GPIO Loopback test execution
 
-    :arg width  : Width of the GPIO under test
-    :arg offset : Offset of the GPIO under test
+    Args:
+            width : Width of the GPIO under test
+            offset : Offset of the GPIO under test
+            logger: Handle for logging
 
     """
-    print("\n Width is: " + str(width))
-    print("\n Offset is: " + str(offset))
+    width = (int(width))/2
+    w_offset = int(offset)
+    r_offset = int(offset) + int(width)
+
+    # Function call to get legitimate GPIO devpath
+    chip = gpio_get_chip()
+    # Function call to generate patterns(1's and 0's)
+    patterns = generate_patterns(width)
+
+    # Write pattern(1's and 0's) of specified width(number of bits) at given offsets
+    # Read pattern(1's and 0's) from given offsets(loopbacked pins)
+    # Perform comparison of both patterns and conclude match/mismatch
+    for w_pattern in patterns:
+        gpio_write_range(chip, w_offset, int(width), w_pattern)
+        r_pattern = gpio_read_range(chip, r_offset, int(width))
+
+        wp = "".join(map(str, w_pattern))
+        rp = "".join(map(str, r_pattern))
+        result = "Match" if rp == wp else "Mismatch"
+
+        logger.info("\nWrite pattern: " + wp + ", Read pattern " + rp + " : " + result)
+
+        assert r_pattern == w_pattern
---
+++ tests/gpio/gpio_config.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2023 Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: MIT
+
+supported_boards = {
+    'kv260': [
+        {'label': 'pmod0', 'width': '8', 'offset': '0'},
+    ],
+
+    'kd240': [
+        {'label': 'pmod0', 'width': '8', 'offset': '0'},
+        {'label': 'gate_drive_en', 'width': '1', 'offset': '8'},
+        {'label': 'brake_cntrl', 'width': '1', 'offset': '9'},
+        {'label': 'one_wire', 'width': '1', 'offset': '10'},
+    ],
+
+    'kr260': [
+        {'label': 'pmod0', 'width': '8', 'offset': '0'},
+        {'label': 'pmod1', 'width': '8', 'offset': '8'},
+        {'label': 'pmod2', 'width': '8', 'offset': '16'},
+        {'label': 'pmod3', 'width': '8', 'offset': '24'},
+        {'label': 'rpi', 'width': '28', 'offset': '32'},
+    ]
+}
+

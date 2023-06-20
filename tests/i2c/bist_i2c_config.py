937d64a0732ab967806e625de31502cb81b79c6b
Harshitha Panduranga <harshith@amd.com>
2023-06-20 15:11:46 -0700

    i2c: Add KD240 config structure
    This commit includes i2c KD240 config structure to the BIST.
    The purpose of the i2c test is to check for the presence of
    i2c devices on the specified bus from configuration file.
    The updated config structure provides reference to verify
    these expected i2c devices for KD240 BIST.

--- tests/i2c/bist_i2c_config.py
+++ tests/i2c/bist_i2c_config.py
@@ -28,6 +28,21 @@ ps_i2c_kr_cc = {
     'i2c_mux'     : 0x74
 }
 
+ps_i2c_k24_som = {
+    'da9062'      : 0x30,
+    'da9131'      : 0x33,
+    'eeprom_som'  : 0x50,
+    'slg7x644092' : 0x68,
+    'slg7x644091' : 0x70
+}
+
+ps_i2c_kd_cc = {
+    'slg7xl45106' : 0x11,
+    'usb5477'     : 0x2d,
+    'ina260'      : 0x40,
+    'eeprom_cc'   : 0x51
+}
+
 supported_boards = {
     'kv260': [
         {
@@ -67,6 +82,14 @@ supported_boards = {
             'controller'  : "ff030000", # Address of the controller
             'mux_channel' : 1,
             'i2c_devices' : {**ps_i2c_k26_som, **ps_i2c_kr_cc, **{'usb5477' : 0x2d}}
+        }
+    ],
+    'kd240': [
+        {
+            'label'       : "ps_i2c_bus_main",
+            'controller'  : "ff030000", # Address of the controller
+            'mux_channel' : None,
+            'i2c_devices' : {**ps_i2c_k24_som, **ps_i2c_kd_cc}
         }
     ]
 }

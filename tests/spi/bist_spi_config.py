48d9893cd107781f548f9def7cfce13ddedb497e
Harshitha Panduranga <harshith@amd.com>
2023-06-28 16:26:41 -0700

    spi: Add SPI test files for Torque sensor
    This commit adds SPI Torque ADC test files to the BIST. The
    purpose of the test is to detect the Torque sensor AD7797 on
    the SPI bus on loading the BIST firmware. The detection is
    done with the help of two separate tests by dynamic device path
    lookup. The run_ad7797_id_test() verifies the Torque sensor
    AD7797 ID and run_ad7797_temperature_test() reads the raw value of
    inbuilt temperature sensor to verify the torque sensor presence.
    The tests use python-periphery spi module to perform SPI
    transactions.

---
+++ tests/spi/bist_spi_config.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2023 Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: MIT
+
+supported_boards = {
+    'kd240': [
+        {
+            'label': 'ad7797_torque_sensor_id_read',
+            'controller': 'a0030000',
+            'channel_select': '0',
+            'spi_device': 'dh2228fv'  # Spi device driver used for the torque sensor AD7797
+        },
+        {
+            'label': 'ad7797_torque_sensor_temperature_read',
+            'controller': 'a0030000',
+            'channel_select': '0',
+            'spi_device': 'dh2228fv'  # Spi device driver used for the torque sensor AD7797
+        }
+    ]
+}
---
+++ tests/spi/conftest.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2023 Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: MIT
+
+# Import the 'modules' that are required for execution
+import pytest
+from bist_spi_config import supported_boards
+
+
+def pytest_generate_tests(metafunc):
+    """
+    Collection and parametrization of tests
+
+    Args:
+            metafunc: Has a parametrize function,way to provide multiple variants of values for parametrization
+
+    Data Structure:
+            val(list): Lookup for board parameters
+            test_id(list): Lookup for Test labels.Tests are parametrized based on label
+
+    """
+    board = metafunc.config.getoption("board")
+    val = []
+    test_id = []
+
+    # Create a list of configs(based on the board) and test IDs for parametrization
+    if board in supported_boards.keys():
+        val = supported_boards[board]
+        for value in supported_boards[board]:
+            test_id.append(value['label'])
+
+    else:
+        val = pytest.skip("Not supported")
+
+    # Parametrize tests based on test IDs
+    metafunc.parametrize("id", val, ids=test_id)

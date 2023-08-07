74d2201587688cb83c33bb1d8e88d607adf54e68
Sagar Karmarkar <sagar.karmarkar@amd.com>
2023-08-07 09:14:24 -0700

    tests:eth: Modified the ethernet test execution method
    Signed-off-by: Sagar Karmarkar <sagar.karmarkar@amd.com>

--- tests/eth/bist_eth.py
+++ tests/eth/bist_eth.py
@@ -2,19 +2,15 @@
 # SPDX-License-Identifier: MIT
 
 import os
+import signal
 import subprocess
 import netifaces
 import ipaddress
-from pyroute2 import IPRoute
 from ping3 import ping
-import iperf3
-import func_timeout
 import glob
 from time import sleep
+import re
 
-remote_host_ip = '192.168.0.1'
-remote_host_sfp_ip = '192.168.0.2'
-static_ip_prefix = '192.168.0.1'
 
 def eth_get_interface_speed(phy_addr, logger):
     """
@@ -107,47 +103,38 @@ def eth_setup(label, eth_interface, logger):
     """
 
     if "sfp" in label:
-        # Override remote_host_sfp_ip if BIST_REMOTE_HOST_SFP_IP env var is defined
-        global remote_host_sfp_ip
-        remote_host_sfp_ip = os.getenv('BIST_REMOTE_HOST_SFP_IP', default=remote_host_sfp_ip)
-        host_ip = remote_host_sfp_ip
-        logger.info("The remote host SFP IP is currently set to " + remote_host_sfp_ip + " "
-            "and can be changed by setting the BIST_REMOTE_HOST_SFP_IP environment variable.")
+        # Check if BIST_REMOTE_HOST_SFP_IP env var is defined
+        host_ip = os.getenv('BIST_REMOTE_HOST_SFP_IP', default=None)
+        if host_ip == None:
+            logger.error("Please set the BIST_REMOTE_HOST_SFP_IP environment variable.")
+            return False, False
+        logger.info("The remote host SFP IP is currently set to " + host_ip)
     else:
-        # Override remote_host_ip if BIST_REMOTE_HOST_IP env var is defined
-        global remote_host_ip
-        remote_host_ip = os.getenv('BIST_REMOTE_HOST_IP', default=remote_host_ip)
-        host_ip = remote_host_ip
-        logger.info("The remote host IP is currently set to " + remote_host_ip + " "
-            "and can be changed by setting the BIST_REMOTE_HOST_IP environment variable.")
+        # Check if BIST_REMOTE_HOST_IP env var is defined
+        host_ip = os.getenv('BIST_REMOTE_HOST_IP', default=None)
+        if host_ip == None:
+            logger.error("Please set the BIST_REMOTE_HOST_IP environment variable.")
+            return False, False
+        logger.info("The remote host IP is currently set to " + host_ip)
 
     # Check if interface already has an IP in the same subnet as remote host
     try:
         interface_ips = [data['addr'] for data in netifaces.ifaddresses(eth_interface)[netifaces.AF_INET]]
     except:
-        logger.info("No interface IP found for " + str(eth_interface))
-        interface_ips = []
+        logger.error("No interface IP found for " + str(eth_interface) + ". Please set a Static IP or use DHCP")
+        return False, False
 
-    if interface_ips != []:
-        logger.debug(str(eth_interface) + " IPs: " + str(interface_ips))
-        # Remove Host ID from interface ip and add /24 subnet mask
-        ip_networks = [".".join(interface_ip.split('.')[:-1]) + '.0/24' for interface_ip in interface_ips]
-        logger.debug("IP networks: " + str(ip_networks))
-        for i in range(len(ip_networks)):
-            if (ipaddress.ip_address(remote_host_ip) in ipaddress.ip_network(ip_networks[i])):
-                logger.debug("Remote host IP " + remote_host_ip + " is in IP network " + ip_networks[i])
-                return interface_ips[i], host_ip
-            else:
-                logger.debug("Remote host IP " + remote_host_ip + " is not in IP network " + ip_networks[i])
-
-    # Assign static IP with last digit matching eth interface
-    interface_ip = static_ip_prefix + eth_interface[-1]
-    logger.info("Adding static IP " + interface_ip)
-    ip = IPRoute()
-    index = ip.link_lookup(ifname=eth_interface)[0]
-    ip.addr('add', index=index, address=interface_ip, prefixlen=24)
-    ip.close()
-    return interface_ip, host_ip
+    logger.debug(str(eth_interface) + " IPs: " + str(interface_ips))
+    # Remove Host ID from interface ip and add /24 subnet mask
+    ip_networks = [".".join(interface_ip.split('.')[:-1]) + '.0/24' for interface_ip in interface_ips]
+    logger.debug("IP networks: " + str(ip_networks))
+    for i in range(len(ip_networks)):
+        if (ipaddress.ip_address(host_ip) in ipaddress.ip_network(ip_networks[i])):
+            logger.debug("Remote host IP " + host_ip + " is in IP network " + ip_networks[i])
+            return interface_ips[i], host_ip
+        else:
+            logger.error("Remote host IP " + host_ip + " is not in IP network " + ip_networks[i])
+            return False, False
 
 def run_eth_ping_test(label, phy_addr, helpers):
     """
@@ -170,6 +157,8 @@ def run_eth_ping_test(label, phy_addr, helpers):
         return False
 
     _, host_ip = eth_setup(label, eth_interface, logger)
+    if not host_ip:
+        return False
 
     logger.info("Pinging remote host " + host_ip)
     ping_result = ping(host_ip, interface=eth_interface)
@@ -180,6 +169,28 @@ def run_eth_ping_test(label, phy_addr, helpers):
         return True
     else:
         return False
+
+
+def get_bitrate(iperf3_output, logger):
+    """
+    Get the average bitrate from iperf3 output
+
+    Args:
+            iperf3_output: iperf3 command line output
+            logger: Handle for logging
+
+    Returns:
+            float: Average value of bitrate
+    """
+    bitrate_list = re.findall(r"(\d+(?:\.\d+)?)\s+Mbits/sec", iperf3_output)
+    if len(bitrate_list) > 0:
+        # Get the last value from list of values which is the average
+        average_bitrate = bitrate_list[-1]
+        return float(average_bitrate)
+    else:
+        logger.error("Bitrate was not recorded")
+        return False
+
 
 def run_eth_perf_test(label, phy_addr, helpers):
     """
@@ -202,33 +213,36 @@ def run_eth_perf_test(label, phy_addr, helpers):
         return False
 
     interface_ip, host_ip = eth_setup(label, eth_interface, logger)
-
-    client = iperf3.Client()
-    client.duration = 1
-    client.server_hostname = host_ip
-    client.port = 5201
-    client.bind_address = interface_ip
-    logger.debug("Client bind address: " + str(client.bind_address))
-    try:
-        perf_timeout = 3 # seconds
-        perf_result = int(func_timeout.func_timeout(perf_timeout, client.run).sent_Mbps)/1000
-    except func_timeout.FunctionTimedOut:
-        sleep(0.05) # Short delay to prevent unwanted prints to console
-        os.dup2(client._stdout_fd, 1) # Redirect output to console
-        logger.error("iperf3 test timed out for interface " + str(eth_interface) + ". "
-            "Please ensure the remote host IP is correct and an iperf3 server is running on the remote host.")
+    if not interface_ip or not host_ip:
         return False
-    except AttributeError as e:
+
+    # iperf3 command,run it for 5 seconds,display rate in Mbits/sec,use zerocopy flag
+    iperf3_cmd = f"iperf3 -c {host_ip} -B {interface_ip} -f m -t 5 -Z"
+    # Run the iperf3 command
+    try:
+        process = subprocess.Popen(iperf3_cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+        stdout, stderr = process.communicate(timeout=10)
+    except subprocess.TimeoutExpired as e:
+        logger.error("iperf3 command timed out after " + str(e.timeout) + " seconds.")
+        os.kill(process.pid, signal.SIGKILL)
+        return False
+    if process.returncode:
         logger.error("iperf3 failed to measure bitrate. "
             "Please ensure the remote host IP is correct and an iperf3 server is running on the remote host.")
+        os.kill(process.pid, signal.SIGKILL)
         return False
-    logger.info("Measured bitrate: " + str(perf_result) + " Gbits/sec")
+    iperf3_output = stdout.decode('utf8')
+    logger.debug(iperf3_output)
 
     # Check if measured bitrate is above threshold
-    perf_speed_threshold = 0.8
-    if perf_result >= perf_speed_threshold*max_speed_gbps:
+    iperf3_result = get_bitrate(iperf3_output, logger)
+    if not iperf3_result:
+        return False
+
+    perf_speed_threshold = 800
+    if iperf3_result >= perf_speed_threshold:
         return True
     else:
-        logger.error("The measured bitrate is lower than " + str(int(perf_speed_threshold*100)) + "% of the "
+        logger.error("The measured bitrate is lower than " + str(int(perf_speed_threshold/10)) + "% of the "
             "max bitrate of " + str(max_speed_gbps) + " Gbits/sec")
         return False
